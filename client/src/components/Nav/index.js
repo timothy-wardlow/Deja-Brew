7582220c04921e170cbf96f700b8c7c5fbc26826
Timothy Wardlow <wardlowtimothy@icloud.com>
2023-02-13 13:53:16 -0600

    still working on updating nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
-import Logo from '../../assets/logo.png';
+import Logo from './../../assets/logo.png';
 
 function Nav() {
 
