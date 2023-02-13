f12ceb601c5b0f1aea0211b785cc430d2e358dd6
Timothy Wardlow <wardlowtimothy@icloud.com>
2023-02-13 14:54:45 -0600

    light roast product animation

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,11 +2,15 @@ import React from "react";
 //import ProductList from "../components/ProductList";
 import Lightroast from "../assets/light-roast.png"
 import Cart from "../components/Cart";
+import 'animate.css';
+import Logo from "../assets/logo.png"
 
 const Home = () => {
   return (<>
-    <div className="row banner" style={{backgroundImage: `url(${Lightroast})`}}>
-
+    <div className="row banner" style={{backgroundImage: `url(${Logo})`}}>
+    <div className='opener'>
+          <img src={Lightroast} alt="Light Roast" className=""  />
+          </div>
     </div>
     <div className="container">
       {/* <CategoryMenu /> */}
