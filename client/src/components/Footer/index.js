4e8b1d8745ddc9e23ba3cc4f6ea0c93fb8b2e72a
Timothy Wardlow <wardlowtimothy@icloud.com>
2023-02-16 19:01:15 -0600

    testing

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 // import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faGithub} from '@fortawesome/free-brands-svg-icons';
-import {faCoffee, faMugHot} from '@fortawesome/free-solid-svg-icons';
+import {faMugHot} from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
 
