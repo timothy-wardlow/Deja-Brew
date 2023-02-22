f77d8d5eeb99a64104908b80ade03a3f70c8979d
Timothy Wardlow <wardlowtimothy@icloud.com>
2023-02-21 19:13:22 -0600

    let's try this again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://timothywardlow:|Kiy32pHsprtBXNO4@deja-brew.x7mgjz3.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://timothywardlow:Kiy32pHsprtBXNO4@deja-brew.x7mgjz3.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
