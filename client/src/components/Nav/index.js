73643602df02c78ed248d6d16ccd679ccc528531
Timothy Wardlow <wardlowtimothy@icloud.com>
2023-02-23 18:23:30 -0600

    fixing typo for logged in users

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -18,7 +18,7 @@ function Nav() {
           <li className="mx-1">
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
             <a href="/" onClick={() => Auth.logout()}>
-              <button>Login</button>
+              <button>Logout</button>
             </a>
           </li>
           <li className="mx-1">
